bba70e45ebf9b55e68f081b176ad5eee1f8b4d7b
Faryal Naeem <faryalnaeem66@yahoo.com>
2020-06-13 14:43:59 +0500

    updated basic react stete

---
+++ src/Room.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Room() {
+    //const state = useState(false);
+    //console.log(state);
+    let [isLit, setLit] = useState(false);
+    let [age, setAge] = useState(24);
+    //function updateLit() {
+      //  console.log ("Button Clicked");
+        //setLit(!isLit);
+    
+
+    return (
+    <div>  
+        This room is {isLit? "lit" : "dark"}
+        <br/>
+        Age: {age}
+        <br/>
+        <button onClick= {()=> setLit(!isLit)} > Toggle Light</button>
+        <br/>
+        <button onClick= {()=> {
+                   // console.log("increaseAge arrow function");
+                    setAge(++age);
+                 }} > Increase Age</button>
+    </div>
+    );
+                }
+  
+export default Room;
