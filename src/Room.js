c200fb1e5e8c9a06757d355cec37882ac9804493
Faryal Naeem <faryalnaeem66@yahoo.com>
2020-06-15 20:40:24 +0500

    Created two buttons,func temp

--- src/Room.js
+++ src/Room.js
@@ -1,30 +1,32 @@
 import React, { useState } from 'react';
-import './Room.css'
+import './Room.css';
 
 function Room() {
     //const state = useState(false);
     //console.log(state);
-    let [isLit, setLit] = useState(false);
-    let [age, setAge] = useState(24);
+    let [isLit, setLit] = useState(true);
+    
+    //let [age, setAge] = useState(24);
+    let [temp,settemp] = useState(72);
     //function updateLit() {
       //  console.log ("Button Clicked");
         //setLit(!isLit);
     
 
     return (
-    <div className ={"room"+(isLit?"lit":"dark")}>  
+    <div className = {`room ${isLit? "lit" : "dark"}`}>  
         This room is {isLit? "lit" : "dark"}
         <br/>
-        Age: {age}
+        Temp:{temp}
         <br/>
-        <button onClick= {()=> setLit(!isLit)} > Toggle Light</button>
+        <button onClick= {()=>setLit(true)} > Toggle On</button>
+        <button onClick= {()=> setLit()} > Toggle off</button>
         <br/>
-        <button onClick= {()=> {
-                   // console.log("increaseAge arrow function");
-                    setAge(++age);
-                 }} > Increase Age</button>
+        <button onClick={()=>settemp(++temp)}> +  </button>
+        <button onClick={()=>settemp(--temp)}> -  </button>
+    
     </div>
     );
-                }
+}
   
 export default Room;
